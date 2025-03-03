b99888e6ffc6e556849b0fd668f5422a77606917
ablan <ablan@remofirst.com>
2025-03-04 02:30:20 +0500

    Implement candidate sign up, job list, job details

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import AuthPage from './pages/auth.page'
 import CompanyPage from './pages/company.page'
 import TalentPage from './pages/talent.page'
+// import CandidateForm from './pages/candidate.page'
+import CandidateProfileForm from './pages/candidate.page'
+import CandidateSignUp from './pages/candidate-signup.page'
+import JobList from './pages/job-list.page'
+import JobDetail from './pages/job-detail.page'
 
 function App() {
 	return (
@@ -11,6 +16,10 @@ function App() {
 				<Route path='/login' element={<AuthPage />} />
 				<Route path='/company' element={<CompanyPage />} />
 				<Route path='/talent' element={<TalentPage />} />
+				<Route path='/candidate' element={<CandidateProfileForm />} />
+				<Route path='/candidate/signup' element={<CandidateSignUp />} />
+				<Route path="/jobs" element={<JobList />} />
+        		<Route path="/jobs/:id" element={<JobDetail />} />
 			</Routes>
 		</Router>
 	)
