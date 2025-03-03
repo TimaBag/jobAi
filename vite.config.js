e90eb9b5927eb7fc5064e406b3611521097c7380
Timur <timur@remofirst.com>
2025-03-04 03:13:34 +0500

    fix: change port

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	plugins: [react()],
-	preview: {
-		port: 8080, // Use Railway's assigned port
-		host: '0.0.0.0',
-	},
 	server: {
-		port: 8080,
-		host: '0.0.0.0',
+		host: true, // Allow external access
+		port: 4173, // Ensure it matches your Railway port
+		strictPort: true,
+		preview: {
+			allowedHosts: ['jobai-production.up.railway.app'],
+		},
 	},
 })
