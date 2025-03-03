b0b6df77721107d4ce48b657621d9047013d8b58
Timur <timur@remofirst.com>
2025-03-04 02:47:04 +0500

    Prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: './', // Ensures proper path handling in Railway
+	base: '/jobai/', // 👈 Add this line
 })
